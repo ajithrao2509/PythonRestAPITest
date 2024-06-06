7a8a05f87ec39bb13154f7a0523028cd0e0dbd08
Ajith KV <ajith@gameopedia.com>
2024-06-06 17:01:41 +0530

    changes

--- test_get_task_get_request.py
+++ test_get_task_get_request.py
@@ -5,7 +5,7 @@ ENDPOINT = config_reader.readConfig("get_task", "url")
 
 
 def test_get_task_rqst():
-    task__id = "task_3a87358032c44c139aee76024da72a72"
+    task__id = "task_a61478517e7346d4a22e88dd0c556fcf"
     response = requests.get(ENDPOINT + f"{task__id}")
     data = response.json()
     # validate status code is 200
